7ab4ac4a43e7c784f540f255f8b87c6ed3dc6cb9
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-13 00:56:25 -0300

    model criada

--- heros_api/src/main/java/com/lucasrmagalhaes/herosapi/HerosapiApplication.java
+++ heros_api/src/main/java/com/lucasrmagalhaes/herosapi/HerosapiApplication.java
@@ -1,13 +1,16 @@
 package com.lucasrmagalhaes.herosapi;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class HerosapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HerosapiApplication.class, args);
-	}
 
+		SpringApplication.run(HerosapiApplication.class, args);
+		System.out.println("super poderes com webflux");
+	}
 }
