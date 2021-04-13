5a01f274addb161a387cc92f3188566026935fde
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-12 22:16:00 -0300

    iniciando o projeto

---
+++ heros_api/src/main/java/com/lucasrmagalhaes/herosapi/HerosapiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrmagalhaes.herosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerosapiApplication.class, args);
+	}
+
+}
---
+++ heros_api/src/test/java/com/lucasrmagalhaes/herosapi/HerosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrmagalhaes.herosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
