945dc04509eaa597ff159cc523e568101cc43de2
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-18 14:37:54 -0300

    heroes

--- heros_api/src/main/java/com/lucasrmagalhaes/heroes/constants/HeroConstant.java
+++
@@ -1,7 +0,0 @@
-package com.lucasrmagalhaes.heroes.constants;
-
-public class HeroConstant {
-    public static final String HEROES_ENDPOINT_LOCAL = "/heroes";
-    public static final String ENDPOINT_DYNAMO = "https://localhost:8000";
-    public static final String REGION_DYNAMO = "us-east-1";
-}
--- heros_api/src/main/java/com/lucasrmagalhaes/heroes/repository/HeroesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lucasrmagalhaes.heroes.repository;
-
-import com.lucasrmagalhaes.heroes.document.Heroes;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
-
-@EnableScan
-public interface HeroesRepository extends CrudRepository<Heroes, String> {}
